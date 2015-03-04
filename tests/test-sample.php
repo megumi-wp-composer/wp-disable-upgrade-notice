512241b55f3bbfe8fa798310bcc7e7d0654288b9
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2015-03-05 00:22:33 +0900

    Update test-sample.php

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -4,8 +4,9 @@ class SampleTest extends WP_UnitTestCase {
 	 * @test
 	 */
 	function disable_version_check() {
-		new Megumi\WP\DisableUpgradeNotice();
-		register();
+		$disable_upgrade_notice = new Megumi\WP\DisableUpgradeNotice();
+		$disable_upgrade_notice->register();
+		
 		$this->assertFalse( has_action( 'wp_version_check', 'wp_version_check' ) );
 		$this->assertFalse( has_action( 'admin_init', '_maybe_update_core' ) );
 		$this->assertSame( 10, has_filter( 'pre_site_transient_update_core', '__return_zero' ) );

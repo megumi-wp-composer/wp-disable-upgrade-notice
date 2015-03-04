8ba98fce582e5bfe1fd8f997617856293de9832d
NishimuraShuhei <shuhei@NishimuraShuhei-no-MacBook-Pro.local>
2015-03-05 00:58:20 +0900

    change file name

--- tests/test-wp-disable-upgrade-notice.php
+++ tests/test-wp-disable-upgrade-notice.php
@@ -6,7 +6,7 @@ class SampleTest extends WP_UnitTestCase {
 	function disable_version_check() {
 		$disable_upgrade_notice = new Megumi\WP\DisableUpgradeNotice();
 		$disable_upgrade_notice->register();
-		
+
 		$this->assertFalse( has_action( 'wp_version_check', 'wp_version_check' ) );
 		$this->assertFalse( has_action( 'admin_init', '_maybe_update_core' ) );
 		$this->assertSame( 10, has_filter( 'pre_site_transient_update_core', '__return_zero' ) );

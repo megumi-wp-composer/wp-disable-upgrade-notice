70bd0e8983d0ef35b1753311837d6332f7257511
NishimuraShuhei <shuhei@NishimuraShuhei-no-MacBook-Pro.local>
2015-03-04 23:56:10 +0900

    add sitayo

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -1,5 +1,4 @@
 <?php
-
 class SampleTest extends WP_UnitTestCase {
 
 	//function testSample() {
@@ -8,5 +7,12 @@ class SampleTest extends WP_UnitTestCase {
 	//}
 	function test_disable() {
 		new Megumi\WP\DisableUpgradeNotice();
+	}
+
+	function disable_version_check() {
+		$this->assertFalse( has_action( 'wp_version_check', 'wp_version_check' ) );
+		$this->assertFalse( has_action( 'admin_init', '_maybe_update_core' ) );
+		$this->assertSame( 10, has_filter( 'pre_site_transient_update_core', '__return_zero' ) );
+		//$this->assertSame( true, DISALLOW_FILE_EDIT );
 	}
 }

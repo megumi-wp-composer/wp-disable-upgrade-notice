fdf98157205cf3fc10db02fe584eeccdb4bdf8ce
NishimuraShuhei <shuhei@NishimuraShuhei-no-MacBook-Pro.local>
2015-03-05 00:20:27 +0900

    new?

--- lib/Megumi/WP/DisableUpgradeNotice.php
+++ lib/Megumi/WP/DisableUpgradeNotice.php
@@ -1,11 +1,6 @@
 <?php
 namespace Megumi\WP;
-
 class DisableUpgradeNotice {
-  public function __construct()
-  {
-
-  }
   public function register() {
     remove_action( 'wp_version_check', 'wp_version_check' );
     remove_action( 'admin_init', '_maybe_update_core' );
--- tests/test-sample.php
+++ tests/test-sample.php
@@ -1,18 +1,13 @@
 <?php
 class SampleTest extends WP_UnitTestCase {
-
-	//function testSample() {
-	//	// replace this with some actual testing code
-	//	$this->assertTrue( true );
-	//}
-	function test_disable() {
-		new Megumi\WP\DisableUpgradeNotice();
-	}
-
+	/**
+	 * @test
+	 */
 	function disable_version_check() {
+		new Megumi\WP\DisableUpgradeNotice();
+		register();
 		$this->assertFalse( has_action( 'wp_version_check', 'wp_version_check' ) );
 		$this->assertFalse( has_action( 'admin_init', '_maybe_update_core' ) );
 		$this->assertSame( 10, has_filter( 'pre_site_transient_update_core', '__return_zero' ) );
-		//$this->assertSame( true, DISALLOW_FILE_EDIT );
 	}
 }
